175c09915080b76fc110256db71cb21ab8ab6389
Luke Harrison <rc0037143@student.riversidecollege.ac.uk>
2019-11-18 14:57:57 +0000

    Added hello world print feature via button 1

--- Hello world/Form1.cs
+++ Hello world/Form1.cs
@@ -16,5 +16,10 @@ namespace Hello_world
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        { 
+                MessageBox.Show("Hello world");
+        }
     }
 }
